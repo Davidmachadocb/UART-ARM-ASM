7dd061f0eacca751cb36b2c8caf1e68539573186
David Machado <78983718+Davidmachadocb@users.noreply.github.com>
2021-03-22 21:38:59 -0300

    meu teste.s

--- test.s
+++ test.s
@@ -4,7 +4,7 @@
 	.equ size_exit, 18
 
 	.balign 1
-	buffer: .byte 2
+	buffer: .byte 1
 
 .text
 .global main
@@ -24,15 +24,12 @@ main:
 	ldr r1, =#0x241 @ parametro para criar arquivo caso não exista e caso exista apagar o conteudo dele e escrever o que tem em input.txt
 	mov r2, #384
 	swi #0
-	
-	@l1:
-	@	cmp r6, #0
-	@	beq exit
-	@	and r1, r6, #1
 
-	@	add r1, r1, #48 		
-		
-		mov r1, #97
+	mov r5,r0
+	
+	l1:
+		and r1, r6, #1
+		add r1, r1, #48		
 			
 		ldr r2, =buffer
 		strb r1, [r2]
@@ -42,11 +39,18 @@ main:
 		ldr r1, =buffer
 		mov r2, #1
 		swi 0
+		
+		cmp r6, #0
+		beq close_file
 	
-	@	lsr r6, #1
-	@	b l1
+		lsr r6, #1
+		b l1
 
-			
+	close_file:
+		@fechar output
+		mov r0, r5
+		mov r7, #6
+		swi #0		
 
 
 	exit:
